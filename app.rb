2e123d0f24bdfe754712ed0464c7c96d042c507f
Luke Taylor <99ltaylor@gmail.com>
2021-09-10 16:41:00 +0100

    redirected to sessions

--- app.rb
+++ app.rb
@@ -6,6 +6,8 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   # get "/" do
   #   # "Testing infrastructure working!"
   # end
@@ -15,8 +17,14 @@ class Battle < Sinatra::Base
   end
 
   post "/names" do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect "/play"
+  end
+
+  get "/play" do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
     erb :play
   end
 
